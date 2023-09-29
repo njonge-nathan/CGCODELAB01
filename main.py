6f94511047aa3ec234490b1271c98d9515661dbf
njonge-nathan <nathanwaithaka24@gmail.com>
2023-09-29 06:46:23 +0300

    Generate Email addresses

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import openpyxl
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Open the Excel file
+excel_file = "Test_Files.xlsx"
+workbook = openpyxl.load_workbook(excel_file)
+sheet = workbook.active
 
+# Define a function to generate email addresses
+def generate_email(student_name):
+    # Remove special characters, spaces, and convert to lowercase
+    cleaned_name = re.sub(r'[^a-zA-Z ]', '', student_name).lower()
+    # Split the name into parts
+    name_parts = cleaned_name.split()
+    if len(name_parts) >= 2:
+        # If there are at least two parts, use the first letter of the first name and the full last name
+        email = f"{name_parts[0][0]}{name_parts[-1]}@gmail.com"
+    else:
+        # If there's only one name part, use the whole name
+        email = f"{name_parts[0]}@gmail.com"
+    return email
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Iterate through the Excel rows, generate email addresses, and write them to the fifth column (column E)
+for row_index, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+    student_name = row[2]  # Assuming the student name is in the third column (index 2)
+    if student_name:
+        email = generate_email(student_name)
+        sheet.cell(row=row_index, column=5, value=email)  # Save email in the fifth column (column E)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Save the workbook with the updated email addresses
+workbook.save("output_data.xlsx")
